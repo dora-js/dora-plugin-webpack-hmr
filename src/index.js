2e105a3e25a50c9029db6a2d8dec39aa3c888c06
sorrycc <sorrycc@gmail.com>
2016-04-07 14:39:29 +0800

    Init Commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import hotMiddleware from 'webpack-hot-middleware';
+import webpack from 'atool-build/lib/webpack';
+import { join } from 'path';
+
+export default {
+
+  'middleware'() {
+    const compiler = this.get('compiler');
+    if (!compiler) {
+      throw new Error('[error] must used together with dora-plugin-webpack');
+    }
+
+    return function* (next) {
+      const middleware = hotMiddleware(compiler);
+      yield middleware.bind(null, this.req, this.res);
+      yield next;
+    };
+  },
+
+  'webpack.updateConfig.finally'(webpackConfig) {
+    const { port } = this;
+    const hotEntry = `webpack-hot-middleware/client?path=http://127.0.0.1:${port}/__webpack_hmr`;
+    // 修改 entry, 加上 webpack-hot-middleware/client
+    webpackConfig.entry = Object.keys(webpackConfig.entry).reduce((memo, key) => {
+      memo[key] = [hotEntry].concat(webpackConfig.entry[key]);
+      return memo;
+    }, {});
+
+    // Hot reload plugin
+    webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+    // Fallback resolve path for npm2
+    webpackConfig.resolve.fallback = webpackConfig.resolve.fallback || [];
+    webpackConfig.resolve.fallback.push(join(__dirname, '../node_modules'));
+
+    return webpackConfig;
+  },
+
+};
---
+++ test/index-test.js
@@ -0,0 +1,32 @@
+import 'babel-polyfill';
+import dora from 'dora';
+import { join } from 'path';
+import request from 'supertest';
+
+const port = '12347';
+
+describe('index', () => {
+
+  const cwd = process.cwd();
+
+  before(done => {
+    process.chdir(join(__dirname, './fixtures/normal'));
+    dora({
+      port,
+      plugins: ['dora-plugin-webpack', '../../../src/index'],
+      cwd: join(__dirname, './fixtures/normal'),
+    });
+    setTimeout(done, 2000);
+  });
+
+  after(() => {
+    process.chdir(cwd);
+  });
+
+  it('GET /package.json', done => {
+    request(`http://localhost:${port}`)
+      .get('/package.json')
+      .expect(/\.\/index\.js/, done);
+  });
+});
+
